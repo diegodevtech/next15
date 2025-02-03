753c4ba2f814a5ceec87fc25f4343467cd90bd72
diegodevtech <diegodevtechnology@gmail.com>
2025-02-03 19:59:44 -0400

    feat: adds counter page and component

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,10 @@
+import { Counter } from './counter'
+
+export const metadata = {
+  title: "Counter",
+  description: "description tesste"
+}
+
+export default function CounterPage() {
+  return <Counter />
+}
