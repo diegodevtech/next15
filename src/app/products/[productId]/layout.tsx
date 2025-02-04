27a16f3054daabbd994519dfd0ace79648a0468c
diegodevtech <diegodevtechnology@gmail.com>
2025-02-04 12:33:39 -0400

    feat: adds a layout config for productId page

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,4 +1,12 @@
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count)
+}
+
 function ProductDetailsLayout({ children }:{children: React.ReactNode}) {
+  const random = getRandomInt(2);
+  if( random === 1 ){
+    throw new Error("Error loading productX")
+  }
   return (
     <div style={{backgroundColor: "red", padding: "4rem"}}>
     <h2 style={{ backgroundColor: "#fff" }}>Featured Products</h2>
