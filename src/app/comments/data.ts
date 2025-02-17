74b6732b001888d774d592e98755ede379689a1d
diegodevtech <diegodevtechnology@gmail.com>
2025-02-17 11:47:05 -0400

    feat: adds some data

---
+++ src/app/comments/data.ts
@@ -0,0 +1,18 @@
+export const comments = [
+  {
+    id: 1,
+    text: "This is the commend number 1"
+  },
+  {
+    id: 2,
+    text: "This is the commend number 2"
+  },
+  {
+    id: 3,
+    text: "This is the commend number 3"
+  },
+  {
+    id: 4,
+    text: "This is the commend number 4"
+  },
+]
