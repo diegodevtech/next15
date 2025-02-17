7e1c341a6b3df01b2374ff14ec0d2256391f0866
diegodevtech <diegodevtechnology@gmail.com>
2025-02-17 09:39:59 -0400

    feat: adds a default page for users route

---
+++ src/app/complex-dashboard/@users/default.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@/app/components/card'
+import React from 'react'
+
+function UserAnalyticsDefault() {
+  return (
+    <Card>UserAnalytics Default Content</Card>
+  )
+}
+
+export default UserAnalyticsDefault
